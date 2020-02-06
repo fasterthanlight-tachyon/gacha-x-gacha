f66e48f2e8e9e17d0c94dd1da57cdb348a223630
Gui Guy <jedandeganwang1999@gmail.com>
2020-02-06 14:34:05 -0600

    Initial commit

---
+++ src/main/java/com/github/crocharisma/gachaxgacha/GachaXGachaApplication.java
@@ -0,0 +1,13 @@
+package com.github.crocharisma.gachaxgacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachaXGachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachaXGachaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/crocharisma/gachaxgacha/GachaXGachaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.crocharisma.gachaxgacha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachaXGachaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
